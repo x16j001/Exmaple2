c878e2f29359eb424f5e86c8d8332dc712329d84
WIN_NT\x16j001 <x16j001@chiba-fjb.ac.jp>
2017-04-28 11:54:38 +0900

    no message

---
+++ Example2/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Example2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcAdd_Click(object sender, EventArgs e)
+        {
+            lblAnsAdd.Text = String.Format("{0}", 1 + 2);
+        }
+
+        private void btnCalcSub_Click(object sender, EventArgs e)
+        {
+            lblAnsSub.Text = String.Format("{0}", 5-3);
+        }
+
+        private void btnCalcMul_Click(object sender, EventArgs e)
+        {
+            lblAnsMul.Text = String.Format("{0}", 2*3);
+        }
+
+        private void btnCalcDiv_Click(object sender, EventArgs e)
+        {
+            lblAnsDiv.Text = String.Format("{0}", 8/2);
+        }
+    }
+}
